cd3e341e2b6b238bcc7cefe6a34701d740a17ba5
Kin <winterheat@gmail.com>
2017-04-03 21:22:19 -0700

    add routes and default route and using it with <Route />

---
+++ dev-note.md
@@ -0,0 +1,3 @@
+
+
+npm install --save react-router@2.0.0-rc5
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from "react-router";
+import routes from "./routes";
+
 
 // import App from './components/app';
 import reducers from './reducers';
@@ -11,6 +13,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
