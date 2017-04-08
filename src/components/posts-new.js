fa704dca23f7252b85e17d88d30408d49c8b7f57
Kin <winterheat@gmail.com>
2017-04-07 20:17:14 -0700

    destructure the configuration objects into the input fields

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -5,22 +5,28 @@ import { reduxForm } from "redux-form";
 class PostsNew extends Component {
   render() {
 
+    // the title, categories, content are configuration objects
     const { fields: { title, categories, content }, handleSubmit } = this.props;  // given to us by redux-form
+
+    console.log(title);
 
     return (
       <form onSubmit={handleSubmit}>
         <h3>Create a new Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          {
+            // the {...title} is destructing of object, so that: for each property, do something like onChange={title.onChange}
+          }
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
