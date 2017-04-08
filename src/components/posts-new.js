8939fb847abe34d636ec4be76e57dd171044d809
Kin <winterheat@gmail.com>
2017-04-07 17:18:29 -0700

    add the wire up to PostsNew component, and the rendered content

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
-export default class PostsNew extends Component {
+
+class PostsNew extends Component {
   render() {
     return (
-      <div>Hi, add a post here</div>
+      <form>
+        <h3>Create a new Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
+
+export default reduxForm({
+  form: "PostsNewForm", // in state, it will be form.PostsNewForm and see the line below
+  fields: ["title", "categories", "content"]  // in state, it will be form.PostsNewForm.title, etc
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from "./posts-reducer";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer   // must use the name 'form' for redux-form
 });
 
 export default rootReducer;
